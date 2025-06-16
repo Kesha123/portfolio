39fbd7d51b25ab9948700425240f11f0c6d0431a
Innokentii Kozlov <innokentii.kozlov@nokia.com>
2025-06-16 10:37:04 +0300

    update links

--- frontend/src/portfolio.js
+++ frontend/src/portfolio.js
@@ -7,7 +7,7 @@ const about = {
   name: 'Innokentii Kozlov',
   role: 'Software Engineer',
   description: 'I am a Cloud Engineer with a strong background in software development. I have experience in building and maintaining cloud infrastructure, CI/CD pipelines, and monitoring systems. I am passionate about learning new technologies and solving complex problems.',
-  resume: 'https://innokentii-home-server.s3.eu-north-1.amazonaws.com/resume.pdf',
+  resume: 'https://drive.google.com/file/d/1pz9vKhOtHr-L97yGOz5Xby2LLBoqla_6/view?usp=sharing',
   social: {
     linkedin: 'https://www.linkedin.com/in/innokentii-kozlov/',
     github: 'https://github.com/Kesha123',
@@ -20,7 +20,7 @@ const projects = [
     description:
       'Personal portfolio website built with React and Material UI.',
     stack: ['JavaScript', 'React', 'Material UI', 'HTML', 'CSS', 'Docker', 'Kubernetes'],
-    sourceCode: 'https://drive.google.com/file/d/1pz9vKhOtHr-L97yGOz5Xby2LLBoqla_6/view?usp=sharing',
+    sourceCode: 'https://github.com/Kesha123/portfolio',
     livePreview: 'https://innokentii-kozlov.com',
   },
 ]
